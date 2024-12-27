6b0a273717c26fec2c00defe035357e10ea2a928
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 <lautaroolmedo77@gmail.com>
2024-12-26 22:53:00 -0300

    added repository layer

--- pkg/repository/user/storage/slice.go
+++ pkg/repository/user/storage/slice.go
@@ -1,11 +1,13 @@
 package storage
 
-import "canchitas-libres/pkg/domain/user"
+import (
+	domain "canchitas-libres/pkg/domain/user"
+)
 
-func (s *Slice) GetAll() ([]user.User, error) {
-	return []user.User{}, nil
+func (s *Slice) GetAll() ([]domain.User, error) {
+	return []domain.User{}, nil
 }
 
-func (s *Slice) GetByID(id int) (*user.User, error) {
+func (s *Slice) GetByID(id int) (*domain.User, error) {
 	return nil, nil
 }
