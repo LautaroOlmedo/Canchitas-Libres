37eaff745a83516a312f07af75d25fe463c5fdab
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 <lautaroolmedo77@gmail.com>
2025-01-02 21:25:36 -0300

    first API

--- pkg/repository/rest/user_handler.go
+++ pkg/repository/rest/user_handler.go
@@ -19,7 +19,6 @@ func NewHandler() *UserHandler {
 func (handler *UserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch {
 	case r.Method == http.MethodPost:
-
 		handler.CreateUser(w, r)
 		return
 	case r.Method == http.MethodGet:
@@ -31,7 +30,6 @@ func (handler *UserHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 }
 
 func (handler *UserHandler) GetUsers(w http.ResponseWriter, r *http.Request) {
-
 	if r.Method == http.MethodGet {
 		w.Header().Set("Content-Type", "application/json") // Seteo en los headers de la respuesta el formato que le quiero dar
 		responseJson, err := json.Marshal(persons)         // Marshea "serealiza" a formato json
@@ -55,7 +53,7 @@ func (handler *UserHandler) CreateUser(w http.ResponseWriter, r *http.Request) {
 		if err != nil {
 			w.WriteHeader(http.StatusBadRequest)
 		}
-		err = json.Unmarshal(data, &person)
+		err = json.Unmarshal(data, &person) // Si cuando envío hago Marshall, cuando recibo hago Unmarshall
 		if err != nil {
 			w.Write([]byte("invalid json"))
 		}
